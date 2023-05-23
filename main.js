4322a5884d71985c2818e6a62eef0f1679ab2cfd
RafaelaGodkeCantro300312 <113138692+RafaelaGodkeCantro300312@users.noreply.github.com>
2023-05-23 17:07:01 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,16 @@
+function preload() {
+
+}
+
+function setup() {
+    canvas=createCanvas(300, 300);
+    canvas.position(500, 180);
+}
+
+function draw() {
+
+}
+
+function takeSnapshot() {
+    save('mySelfie.png');
+}
